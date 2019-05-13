9c3e3b7a6ce0e243ba77db3eb3eb10016658713f
Raymond Thurman <raymondthurman5@gmail.com>
2019-05-12 20:17:39 -0400

    fixed my if statement. MVP complete

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -4,8 +4,8 @@ class TabLink {
     this.tabElement = tabElement;
 
     // Get the `data-tab` value from this.tabElement and store it here
-    this.tabData = document.querySelector(`.tab[data-tab='${this.tabElement.dataset.tab}']`);
-    console.log(this.tabData);
+    this.tabData = this.tabElement.dataset.tab;
+
     // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:    
 
 
@@ -13,23 +13,22 @@ class TabLink {
     if (this.tabData === 'all') {
       // If `all` is true, select all cards regardless of their data attribute values
       this.cards = document.querySelectorAll('.card');
+
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
-      this.cards = this.tabData;
+      this.cards = document.querySelectorAll(`.card[data-tab='${this.tabData}']`);
     }
 
-    console.log(this.cards);
-    // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
-    this.cards = Array.from(this.cards).map(card => {
-      new TabCard(card);
-    });
+    // // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
+    this.cards = Array.from(this.cards).map(card => new TabCard(card));
+
 
     // Add a click event that invokes this.selectTab
     this.tabElement.addEventListener('click', () => this.selectTab());
   }
 
   selectTab() {
-
+    console.log(this.cards);
     // Select all elements with the .tab class on them
     const tabs = document.querySelectorAll('.tab');
 
